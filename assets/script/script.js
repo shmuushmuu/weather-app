475ef43b8bde87a1835fd7d51c3db268023b5870
Sam Craig <samfcraig@gmail.com>
2022-12-13 19:47:12 -0500

    filled out geocoded api and created handle search function

--- assets/script/script.js
+++ assets/script/script.js
@@ -8,25 +8,36 @@
 // WHEN I click on a city in the search history
 // THEN I am again presented with current and future conditions for that city
 
-fetch(apiUrl)
-    .then(function (response){
-        return response.json();
-    })
-    .then(function (data){
-        console.log(data);
-    })
-    .catch(function (error){
-        console.log(error);
-    })
-
-    //today's weather
-apiUrl = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
-
-// geocoding
-geocodingApiUrl = "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}";
-
-// 5 day weather
-apiWeekUrl = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
-
 //my Api key
 //3e1945513cc5bde07da51feba3091383;
+
+//today's weather
+//apiUrl = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
+
+// geocoding
+//geocodingApiUrl = "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}";
+
+// 5 day weather
+//apiWeekUrl = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
+
+var searchedCity = document.getElementById('#search');
+
+function fetchWeather(currentCity) {
+    var apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + q + "&limit=5&appid=3e1945513cc5bde07da51feba3091383";
+
+    fetch(apiUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+}
+var handleSearch = function (event) {
+        event.preventDefault();
+        var currentCity = searchedCity.value.trim();
+        fetchWeather(currentCity);
+}
